2593b52ce8121622370c2604a6d31a1aba5328b8
HanabiGuo <894577708@qq.com>
2015-05-03 16:33:31 +0800

    commit

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,6 @@
 javascript:(function(d){
 s = d.createElement('script');
-s.setAttribute('src', 'http://hanabiguo.github.io/javascripts/punlock.js');
+s.setAttribute('src', 'http://hanabiguo.github.io/pixiv_unlock/assets/punlock.js');
 s.setAttribute('charset', 'utf-8');
 d.getElementsByTagName('head')[0].appendChild(s);
 })(document)
